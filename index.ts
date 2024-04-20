5c7f617451d4cb1296ee027df1a1d61ec5befcfa
yMarceloMaia <marcelomaiajp@gmail.com>
2024-04-20 16:52:37 -0300

    feat: Extracted the main information from each product sheet

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer-core')
-const chromium = require("@sparticuz/chromium");
+const chromium = require('@sparticuz/chromium');
+const cheerio = require('cheerio');
 
 const url = "https://www.amazon.com.br/bestsellers)"
 
@@ -16,14 +17,61 @@ module.exports.scraper = async (event: any) => {
     const page = await browser.newPage();
     await page.goto(url);
 
-    const pageTitle = await page.title();
+    const html = await page.content();
+    // const bodyHTML = await page.$eval('body', (body: { innerHTML: any; }) => body.innerHTML);
+    // const bodyHTML = await page.evaluate(() => document.getElementById('zg_left_col1')?.innerHTML);
+    // const pageTitle = await page.title();
+
+    // const products = await page.evaluate((body: any) => {
+    //   const productsPage = document.getElementById('zg_left_col1')
+
+    //   return productsPage?.innerHTML
+    // })
+    // console.log('=================')
+    // console.log(products)
+    // console.log('=================')
+
+    const productsHtml = await page.$$eval('.a-carousel', (items: any[]) => {
+      return items.map(item => {
+        const cardHtml = item.querySelector('.a-carousel-card').innerHTML;
+        return cardHtml;
+      });
+    });
+
+    const products: any = [];
+
+    productsHtml.forEach((item: any) => {
+      const $ = cheerio.load(item);
+      const title = $('div.p13n-sc-truncate-desktop-type2').text();
+      const price = $('span._cDEzb_p13n-sc-price_3mJ9Z').text();
+      const urlImg = $('img').attr('src');
+
+      const product = {
+        title: title.trim(),
+        price: price.trim(),
+        urlImg: urlImg.trim()
+      };
+
+      products.push(product);
+    });
+
+
+    console.log(products);
+
+    // const $ = cheerio.load(bodyHTML);
+
+    // Use os seletores do Cheerio para extrair informações específicas
+    // const pageTitle = $('title').text();
+
+    // console.log(pageTitle);
+    // console.log(bodyHTML);
 
     await browser.close();
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        pageTitle,
+        products: 'ok'
       }),
     };
   } catch (error: any) {
