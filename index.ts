741c3500abbd92423a0ae673c7e549f56c459c3d
yMarceloMaia <marcelomaiajp@gmail.com>
2024-04-20 12:57:40 -0300

    feat: web scraper starting

--- index.ts
+++ index.ts
@@ -1,13 +1,37 @@
-module.exports.handler = async (event: any) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'hello worlddddd!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const puppeteer = require('puppeteer-core')
+const chromium = require("@sparticuz/chromium");
+
+const url = "https://www.amazon.com.br/bestsellers)"
+
+module.exports.scraper = async (event: any) => {
+  try {
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const pageTitle = await page.title();
+
+    await browser.close();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        pageTitle,
+      }),
+    };
+  } catch (error: any) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
 };
